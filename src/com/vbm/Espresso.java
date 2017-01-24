3a3d40e3afea5b4a5dfc41fa4618cfe58be183e4
vbmchik <38gjgeuftd>
2017-01-24 19:56:48 +0300

    КОмментарии

--- src/com/vbm/Espresso.java
+++ src/com/vbm/Espresso.java
@@ -6,7 +6,8 @@ package com.vbm;
 
 class Espresso {
 
-    //sin(x) Calculates the quantity of Symbol in current String pattern
+    // Calculates the quantity of Symbol in current String pattern
+    // Возвращает сколько заданных символов находится в данной строке
     private static int countSymbol(char symbol, String pattern) {
         int countS = 0;
         for (int i = 0; i < pattern.length(); ++i)
@@ -15,12 +16,15 @@ class Espresso {
     }
 
     // If the number of open and close bracets are equal
+    // Возвращает соответствует ли количество открывающих скобок количеству закрывающих
     private static boolean ifBracketsEqual(String pattern) {
         return (countSymbol('(', pattern) == countSymbol(')', pattern));
     }
 
     // Index of first or last found character in the string
     // if fromBegin is true returns index of first character equals symbol, else of the last and -1 if no character found.
+    // Индекс первого с начала или с конца строки заданного символа fromBegin = true - с начала иначе с конце - возвращает
+    // -1 - если такого символа нет
     private static int indexOfCharacter(char symbol, String patternString, boolean fromBegin) {
         int index = -1;
         int flag = 0;
@@ -36,6 +40,7 @@ class Espresso {
     }
 
     // Index of arythmetical operator
+    // Индекс арифметического оператора
     private static int indexOfOperator(String pattern) {
         int indOp;
         if ((indOp = indexOfCharacter('+', pattern, false)) != -1) return indOp;
@@ -47,6 +52,7 @@ class Espresso {
     }
 
     // if we have open and close bracets on the whole expression like (2+3) remove bracets (2+3)-(4-5) - keep
+    // Если выражание целиком закрыто скобками - то убираем скобки
     private static String removeBrackets(String patternString) {
         int indOp = indexOfOperator(patternString);
         if (patternString.length() > 1 && indOp == -1)
@@ -56,11 +62,13 @@ class Espresso {
     }
 
     // Remove some string from given string
+    // Убираем название функции из строки sin(строка) => строка
     private static String cutPattern(String pattern, int length) {
         return pattern.substring(length, pattern.length() - 1);
     }
 
-    // The main part - recurse calculations
+    // The main part - find the value
+    // Вычисляем значанеи выражения
     private static double mathOperator(String pattern) throws Exception {
         double sum = 0;
         String[] parts;
@@ -131,6 +139,7 @@ class Espresso {
     }
 
     // For switch enumarates functions
+    // В зависимости от названия функции получаем ее индекс для switch
     private static int switchOperator(String pattern) {
 
         if (pattern.equalsIgnoreCase("e")) return 1;
@@ -146,7 +155,8 @@ class Espresso {
         return 0;
     }
 
-    public static double calculus(String pattern) throws Exception {
+    // Рекурсивная функция и точка входа
+    static double calculus(String pattern) throws Exception {
         pattern = pattern.replace(" ", "");
         if (pattern.charAt(0) == '-') return (calculus("0" + pattern));
         pattern = removeBrackets(pattern);
